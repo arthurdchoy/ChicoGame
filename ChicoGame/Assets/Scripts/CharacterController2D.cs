1aaa00a85a4f9bb660e05e7abd3aaef72bf17cf0
DaDanielL <danieldaolin.college@gmail.com>
2022-10-23 15:05:02 -0700

    revamped movement implemented colliders

---
+++ ChicoGame/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
+
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = false;  // For determining which way the player is currently facing.
+	private Vector3 m_Velocity = Vector3.zero;
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	public void Move(float hmove, float vmove)
+	{
+		Vector3 targetVelocity;
+
+		// Move the character by finding the target velocity
+		if (hmove != 0 && vmove != 0)
+        {
+			targetVelocity = new Vector2(hmove * 7.5f, vmove * 7.5f);
+        }
+        else
+        {
+			targetVelocity = new Vector2(hmove * 10f, vmove * 10f);
+		}
+		// And then smoothing it out and applying it to the character
+		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+		// If the input is moving the player right and the player is facing left...
+		if (hmove > 0 && !m_FacingRight)
+		{
+			// ... flip the player.
+			Flip();
+		}
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if (hmove < 0 && m_FacingRight)
+		{
+			// ... flip the player.
+			Flip();
+		}
+
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
+
