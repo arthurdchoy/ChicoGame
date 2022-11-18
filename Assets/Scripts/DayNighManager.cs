ee63625c474a694a1e33345745d7a31a01345562
menthy-wu <wumengxuan0728@gmail.com>
2022-11-17 17:25:39 -0800

    day night cycle

---
+++ Assets/Scripts/DayNighManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.Experimental.Rendering.Universal;
+
+public class DayNighManager : MonoBehaviour
+{
+    public Gradient lightColor;
+    public float tick = 1;
+    public float seconds = 0; 
+    public int mins = 0;
+    public int minsPerDay = 1440;
+    public int dayTime = 360;
+    public int nightTime = 1200;
+    public bool isday = false;
+    public int days = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeColor();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeGoes();
+    }
+    private void changeColor()
+    {
+        float timePercent = (float)mins/(float)minsPerDay;
+        gameObject.GetComponent<Light2D>().color = lightColor.Evaluate(timePercent);
+    }
+    private void timeGoes()
+    {
+        seconds += Time.fixedDeltaTime * tick; 
+        if (seconds >= 60) // 60 sec = 1 min
+        {
+            seconds = 0;
+            mins += 1;
+            changeColor();
+        }
+        if(mins==dayTime)
+        {
+            isday = true;
+        }
+        if(mins == nightTime)
+        {
+            isday = false;
+        }
+        if(mins == minsPerDay)
+        {
+            mins = 0;
+            days++;
+        }
+    }
+}
