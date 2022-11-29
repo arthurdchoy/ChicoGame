d40ecf2a977e72b00be04c115c7ee563b6bc38ce
ac2860356 <61389894+ac2860356@users.noreply.github.com>
2022-11-28 23:06:56 -0800

    Added field manager

---
+++ Assets/Scripts/FieldManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FieldManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap fieldMap;
+
+    private Dictionary<Vector3Int, int> fieldTiles = new Dictionary<Vector3Int, int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
